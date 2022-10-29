cd8bed52f84f673879fe6bc2da1c435d088f386c
Mamadou seydou DIALLO <diallodiery301gmail.com>
2022-10-29 03:14:09 +0000

    library d3.js added

--- README.md
+++ README.md
@@ -1,27 +1,13 @@
-# Fuse - Admin template and Starter project for Angular
+# Angular 14 - D3.js
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli)
+## Install angular dependencies (nodes_modules)
 
-## Development server
+Run `npm install` or  `npm install`
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+## Install D3.js
 
-## Code scaffolding
+Run `npm install d3 && npm install @types/d3 --save-dev ` you can also download the latest release on GitHub (`7.6.1`)
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Type definitions will allow TypeScript to apply type hints to the external D3 code.
 
-## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice.  To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,37 +3,65 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
+    },
+    {
+        id: 'd3.graph-one',
+        title: 'Graph One',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/d3'
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id: 'example',
+        title: 'Acceuil',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
+    },
+    {
+        id: 'd3.graph-one',
+        title: 'Graph One',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/d3'
     }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
+    },
+    {
+        id: 'd3.graph-one',
+        title: 'Graph One',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/d3'
     }
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id: 'example',
         title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/example'
+    },
+    {
+        id: 'd3.graph-one',
+        title: 'Graph One',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/d3'
     }
 ];
